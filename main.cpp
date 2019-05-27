d322206350d8815086d55d2afd5b42dfe7a49dd1
mobinsheng <mobinsheng@mobinshengdeMac-mini.local>
2019-05-27 11:07:31 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "bitbuffer.h"
+using namespace std;
+
+int main()
+{
+    uint8_t buffer[512] = {0};
+
+    rtc::BitBuffer reader(buffer,512);
+    rtc::BitBufferWriter writer(buffer,512);
+
+    writer.WriteUInt8(3);
+    writer.WriteUInt32(59);
+
+    uint8_t v8;
+    uint32_t v32;
+    reader.ReadUInt8(&v8);
+    reader.ReadUInt32(&v32);
+
+    cout << v8 << endl;
+    cout << v32 << endl;
+    return 0;
+}
